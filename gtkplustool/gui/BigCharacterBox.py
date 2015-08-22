8b0bb564209df7b6f414c5fdc8447953e17ffbc1
Zelphir Kaltstahl <zelphirkaltstahl@gmail.com>
2015-08-22 18:20:40 +0200

    + began implementing the big character box

--- gtkplustool/gui/BigCharacterBox.py
+++ gtkplustool/gui/BigCharacterBox.py
@@ -5,8 +5,18 @@ __author__ = 'xiaolong'
 
 
 class BigCharacterBox(Gtk.Grid):
+    characters_label = None
+    big_character_label = None
+    previous_button = None
+    next_button = None
+
     def __init__(self):
         super().__init__()
 
+        self.add_widgets()
+
     def add_widgets(self):
-        pass
+        self.characters_label = Gtk.Label('Characters')
+        self.big_character_label = Gtk.Label('中')
+        self.previous_button = Gtk.Button('<<')
+        self.next_button = Gtk.Button('<<')
